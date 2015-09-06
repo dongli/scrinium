02c60d38e1f0bda8a062936932b57a5a50fe370d
Li Dong <dongli@lasg.iap.ac.cn>
2015-09-06 21:55:08 +0800

    解决一些收藏功能在engine中使用的问题
    假设主应用名为Scrinium，那么子应用名为如ScriniumEsm，并且挂载在/esm下。

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,7 +5,7 @@ class CollectionsController < ApplicationController
   def collect
     @collection = @user.collections.new(collection_params)
     # NOTE: 需要和environment.rb中的回调函数保持一致！
-    MessageBus.subscribe "/update-#{@collectable.class}-#{@collectable.id}" do |msg|
+    MessageBus.subscribe "/update-#{CollectionsHelper.class_escape @collectable}-#{@collectable.id}" do |msg|
       collectable_type, collectable_id = msg.channel.split('-')[1,2]
       collection = current_user.collections.find_by_collectable_id_and_collectable_type(
         collectable_id.to_i, collectable_type
@@ -28,7 +28,7 @@ class CollectionsController < ApplicationController
     @collection = @user.collections.find_by_collectable_id_and_collectable_type(
       @collectable.id, @collectable.class
     )
-    MessageBus.unsubscribe "/update-#{@collectable.class}-#{@collectable.id}"
+    MessageBus.unsubscribe "/update-#{CollectionsHelper.class_escape @collectable}-#{@collectable.id}"
     @collection.destroy
     respond_to do |format|
       format.js
@@ -56,16 +56,24 @@ class CollectionsController < ApplicationController
       end
     end
     respond_to do |format|
-      format.html { redirect_to url_for([@collection.collectable.user, @collection.collectable]) }
+      # 因为collectable对象可能位于其它engine。如果是，那么需要用该engine的路由器。
+      match = @collection.collectable_type.match('(\w+)::')
+      app = match ? eval(match[1].underscore) : main_app
+      format.html { redirect_to app.url_for([@collection.collectable.user, @collection.collectable]) }
     end
   end
 
   private
 
   def load_user_and_collectable
-    user_id, resource, collectable_id = request.path.split('/')[2,4]
+    user_id, collectable_type, collectable_id = request.path.match(/\/users\/(\d+)\/(\w+)\/(\d+)/)[1,3]
+    # NOTE: 这里要求所有engine命名规则为<主应用名><子应用名>，并且挂在路径为'/<子应用名>'。
+    engine = request.path.match(/^\/(\w+)/)[1]
+    if engine != 'users'
+      collectable_type = Rails.app_class.to_s.split('::').first+engine.capitalize+'::'+collectable_type.singularize.classify
+    end
     @user = User.find(user_id)
-    @collectable = resource.singularize.classify.constantize.find(collectable_id)
+    @collectable = collectable_type.constantize.find(collectable_id)
   end
 
   def collection_params
--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -5,4 +5,13 @@ module CollectionsHelper
        collectable.id,        collectable.class
     ).empty?
   end
+
+  def class_escape collectable
+    # 因为MessageBus不认'::'，因此需要将'::'转换为其它字符（由class_escape转换）。
+    collectable.class.to_s.gsub('::', '-')
+  end
+
+  def self.class_escape collectable
+    collectable.class.to_s.gsub('::', '-')
+  end
 end
