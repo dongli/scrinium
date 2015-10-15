5285f831cfd79a28099e913c2a3e0c2092436cfb
Li Dong <dongli@lasg.iap.ac.cn>
2015-10-15 13:20:51 +0800

    稍微修改collection
    本打算采用标准RESTful接口，但是觉得还不如collect和uncollect方便。

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,31 +1,27 @@
 class CollectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_user_and_collectable, except: [:toggle_watched, :view]
+  before_action :load_app_and_collectable, except: [ :toggle_watched, :view ]
 
   def collect
-    @collection = @user.collections.new(collection_params)
-    # NOTE: 需要和environment.rb中的回调函数保持一致！
-    MessageBus.subscribe "/update-#{CollectionsHelper.class_escape @collectable}-#{@collectable.id}" do |msg|
-      collectable_type, collectable_id = msg.channel.split('-')[1,2]
-      collection = current_user.collections.find_by_collectable_id_and_collectable_type(
-        collectable_id.to_i, collectable_type
-      )
-      collection.updated = true
-      if not collection.save
-        # TODO: 处理错误。
-      end
-    end
+    @collection = current_user.collections.new(collection_params)
     respond_to do |format|
-      if @collection.save
+      if @collection.save!
+        # NOTE: 需要和environment.rb中的回调函数保持一致！
+        MessageBus.subscribe "/update-#{CollectionsHelper.class_escape @collectable}-#{@collectable.id}" do |msg|
+          collectable_type, collectable_id = msg.channel.split('-')[1,2]
+          collection = current_user.collections.find_by_collectable_id_and_collectable_type(
+            collectable_id.to_i, collectable_type
+          )
+          collection.updated = true
+          collection.save!
+        end
         format.js
-      else
-        # TODO: 处理错误！
       end
     end
   end
 
   def uncollect
-    @collection = @user.collections.find_by_collectable_id_and_collectable_type(
+    @collection = current_user.collections.find_by_collectable_id_and_collectable_type(
       @collectable.id, @collectable.class
     )
     MessageBus.unsubscribe "/update-#{CollectionsHelper.class_escape @collectable}-#{@collectable.id}"
@@ -59,28 +55,29 @@ class CollectionsController < ApplicationController
       # 因为collectable对象可能位于其它engine。如果是，那么需要用该engine的路由器。
       match = @collection.collectable_type.match('(\w+)::')
       app = match ? eval(match[1].underscore) : main_app
-      format.html { redirect_to app.url_for([@collection.collectable.user, @collection.collectable]) }
+      format.html { redirect_to [ app, @collection.collectable ] }
     end
   end
 
   private
 
-  def load_user_and_collectable
-    user_id, collectable_type, collectable_id = request.path.match(/\/users\/(\d+)\/(\w+)\/(\d+)/)[1,3]
-    # NOTE: 这里要求所有engine命名规则为<主应用名><子应用名>，并且挂在路径为'/<子应用名>'。
-    engine = request.path.match(/^\/(\w+)/)[1]
-    if engine != 'users'
-      collectable_type = Rails.app_class.to_s.split('::').first+engine.capitalize+'::'+collectable_type.singularize.classify
+  def load_app_and_collectable
+    # [/<engine prefix>]/<collectable_type>/:id/<collect|uncollect>/...
+    tokens = request.path.split('/').reject(&:empty?)
+    n = tokens.index('collect') || tokens.index('uncollect')
+    collectable_type, collectable_id = tokens[n-2..n-1]
+    if RailsEnginesHelper.engine_names.index { |x| x =~ /_#{tokens.first}$/ }
+      app_name = Rails.app_class.to_s.split('::').first
+      collectable_type = app_name+tokens.first.capitalize+'::'+collectable_type.singularize.classify
     else
       collectable_type = collectable_type.singularize.classify
     end
-    @user = User.find(user_id)
     @collectable = collectable_type.constantize.find(collectable_id)
   end
 
   def collection_params
     params[:collection] = {
-      user_id: @user.id,
+      user_id: current_user.id,
       collectable_id: @collectable.id,
       collectable_type: @collectable.class.to_s
     }
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,44 +1,34 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :load_app_and_commentable
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = @commentable.comments
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
   end
 
-  # GET /comments/new
   def new
     @comment = @commentable.comments.new
   end
 
-  # GET /comments/new/1
   def reply
     @comment = @commentable.comments.new
     @comment.parent_id = params[:id]
   end
 
-  # GET /comments/1/edit
   def edit
     respond_to do |format|
       format.js
     end
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = @commentable.comments.new(comment_params)
-
     respond_to do |format|
-      if @comment.save
+      if @comment.save!
         if @comment.parent
           if @comment.user != @comment.parent.user
             what = t('scrinium.comment')
@@ -69,14 +59,10 @@ class CommentsController < ApplicationController
         end
         MessageBus.publish "/comment-#{@commentable.class.to_s}-#{@commentable.id}", { user_id: @comment.user_id }
         format.js
-      else
-        # TODO: 处理错误。
       end
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update!(comment_params)
@@ -85,8 +71,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
@@ -112,17 +96,10 @@ class CommentsController < ApplicationController
     @commentable = commentable_type.constantize.find(commentable_id)
   end
 
-  def commentable_path
-    resource, id = request.path.split('/')[1,2]
-    "/#{resource}/#{id}"
-  end
-
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = @commentable.comments.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.require(:comment).permit(:commentable_id,
                                     :commentable_type,
