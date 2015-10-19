6150e632351c16e4379692a9a639d409994f4ff8
tianlu1677 <tianlu1677@gmail.com>
2015-10-20 00:50:06 +0800

    实现用户邮箱注册与登录

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_attached_file :avatar, styles: { medium: '150x150', thumb: '100x100', small: '20x20' }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
