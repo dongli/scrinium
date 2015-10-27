ad8a328e94a5a788aefdd37e666e873550a6812e
tianlu1677 <tianlu1677@gmail.com>
2015-10-27 21:40:20 +0800

    打开用户注册时发送邮件

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable :trackable,
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable#, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   acts_as_messageable
 
