c7b94102f0a59db4f283909aa1ebec1deaf44327
tianlu1677 <tianlu1677@gmail.com>
2015-11-03 22:57:03 +0800

    增加操作成功失败的提示

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ApplicationController < ActionController::Base
 
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -13,6 +12,49 @@ class Admin::ApplicationController < ActionController::Base
 
   def index
     @q = resource_class.search(params[:q])
+  end
+
+  def create
+    create! do |success, failure|
+      success.html do
+        flash[:notice] = t('message.create_success', thing: thing)
+        redirect_to(resource_url)
+      end
+
+      failure.html do
+        flash[:error] =  t('message.create_fail', thing: thing)
+        render :action => :new
+      end
+    end
+  end
+
+
+  def update
+    update! do |success, failure|
+      success.html do
+        flash[:notice] = t('message.update_success', thing: thing)
+        redirect_to(resource_url)
+      end
+
+      failure.html do
+        flash[:error] = t('message.update_fail', thing: thing)
+        render :action => :edit
+      end
+    end
+  end
+
+  def destroy
+    destroy! do |success, failure|
+      success.html do
+        flash[:notice] = t('message.destroy_success', thing: thing)
+        redirect_to(resource_url)
+      end
+
+      failure.html do
+        flash[:error] = t('message.destroy_fail', thing: thing)
+        render :action => :edit
+      end
+    end
   end
 
   helper_method :attributes, :permitted_attributes
@@ -36,6 +78,10 @@ class Admin::ApplicationController < ActionController::Base
 
   def attributes
     resource_class.attribute_names - %w(id created_at updated_at )
+  end
+
+  def thing
+    t("activerecord.models.#{resource_class.to_s.underscore}")
   end
 
 
