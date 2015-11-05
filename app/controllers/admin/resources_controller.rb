db7c722a6e7f7ec42feaebe2c3a1df546c7e0d4b
tianlu1677 <tianlu1677@gmail.com>
2015-11-05 22:57:39 +0800

    修改后台输入界面的样式

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -1,2 +1,7 @@
 class Admin::ResourcesController < Admin::ApplicationController
+
+
+  def permitted_attributes
+    %w(user_id resourceable_id name description file resourceable_type status)
+  end
 end
