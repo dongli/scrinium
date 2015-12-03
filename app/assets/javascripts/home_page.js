8dba3d9ff34fde6aa32f49e02742c250a45f4981
Li Dong <dongli@lasg.iap.ac.cn>
2015-12-03 20:56:48 +0800

    初步添加Startup框架
    根据需要再添加相应的css，js等文件吧。

---
+++ app/assets/javascripts/home_page.js
@@ -0,0 +1,3 @@
+//= require startup/common-files/modernizr.custom
+//= require startup/common-files/jquery.sharrre.min
+//= require startup/common-files/startup-kit
--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -53,7 +53,7 @@ class ReferencesController < ApplicationController
 
   def reference_params
     params[:reference][:authors].delete_if { |a| a.empty? }
-    params[:reference][:editors].delete_if { |a| a.empty? }
+    params[:reference][:editors].delete_if { |a| a.empty? } if params[:reference].has_key? :editors
     if not ( @reference and @reference.cite_key )
       # TODO: I only handled <first_name last_name> case. There are other cases!
       last_name = params[:reference][:authors].first.split.last
