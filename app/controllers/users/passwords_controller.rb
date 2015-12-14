42d03b6836aaeb47cd2395de5a1199d3c8669da5
tianlu1677 <tianlu1677@gmail.com>
2015-12-14 22:54:08 +0800

    调整登陆注册样式的示例

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,5 @@
 class Users::PasswordsController < Devise::PasswordsController
+  layout 'home_page'
   # GET /resource/password/new
   # def new
   #   super
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
+  layout 'home_page'
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
