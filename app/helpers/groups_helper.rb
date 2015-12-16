55b984528142196defc382979ecda44f23c169a1
Li Dong <dongli@lasg.iap.ac.cn>
2015-12-16 11:33:17 +0800

    清理程序 #2
    修改一些button显示。
    修改current_user.groups。

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,4 +2,8 @@ module GroupsHelper
   def is_in_groups? groups
     current_user and not (current_user.groups & groups).empty?
   end
+
+  def get_valid_groups_of user
+    user.memberships.where(host_type: 'Group', status: 'approved').map { |x| x.host }
+  end
 end
