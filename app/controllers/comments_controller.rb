88fd34a368c2e2d506d0cb8d315baaa26e6997b8
Li Dong <dongli@lasg.iap.ac.cn>
2015-12-16 10:56:54 +0800

    清理程序 #1
    将engine相关的先去除，以后再考虑更好的实现方式。
    评论不再发站内信，一方面程序复杂度高，另一方面评论多了，邮箱会被挤爆，考虑其它更好的通知方式。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
-  before_action :load_app_and_commentable
+  before_action :load_commentable
   before_action :set_comment, only: [ :show, :edit, :update, :destroy ]
 
   def index
@@ -29,35 +29,7 @@ class CommentsController < ApplicationController
     @comment = @commentable.comments.new(comment_params)
     respond_to do |format|
       if @comment.save!
-        if @comment.parent
-          if @comment.user != @comment.parent.user
-            what = t('scrinium.comment')
-            who = @comment.user.name
-            url = @app.url_for([ @commentable ])+"#comment-#{@comment.id}"
-            subject = t('comment.got_commented_subject', what: what)
-            body = t('comment.got_commented_body', who: who, what: what, url: url)
-            @comment.parent.user.notify subject, body
-            MessageBus.publish "/mailbox-#{@comment.parent.user_id}", { user_id: @comment.user_id }
-          end
-        else
-          if @comment.user != @commentable.user
-            # what的额外逻辑是为了处理engine中的commentable。
-            class_name = @commentable.class
-            if class_name.parent_name
-              namespace = class_name.parent_name.to_s.underscore
-            else
-              namespace = Rails.application.class.parent_name.to_s.underscore
-            end
-            what = t(namespace+'.'+class_name.to_s.split('::').last.underscore)
-            who = @comment.user.name
-            url = @app.url_for([ @commentable ])+"#comment-#{@comment.id}"
-            subject = t('comment.got_commented_subject', what: what)
-            body = t('comment.got_commented_body', who: who, what: what, url: url)
-            @commentable.user.notify subject, body
-            MessageBus.publish "/mailbox-#{@commentable.user.id}", { user_id: @comment.user_id }
-          end
-        end
-        MessageBus.publish "/comment-#{@commentable.class.to_s}-#{@commentable.id}", { user_id: @comment.user_id }
+        MessageBus.publish "/comment-#{@commentable.class.to_s}-#{@commentable.id}", user_id: @comment.user_id
         format.js
       end
     end
@@ -80,19 +52,10 @@ class CommentsController < ApplicationController
 
   private
 
-  def load_app_and_commentable
-    # [/<engine prefix>]/<commentable_type>/:id/comments/...
-    tokens = request.path.split('/').reject(&:empty?)
-    n = tokens.index('comments')
-    commentable_type, commentable_id = tokens[n-2..n-1]
-    if RailsEnginesHelper.engine_names.index { |x| x =~ /_#{tokens.first}$/ }
-      app_name = Rails.app_class.to_s.split('::').first
-      commentable_type = app_name+tokens.first.capitalize+'::'+commentable_type.singularize.classify
-      @app = eval("#{app_name.downcase}_#{tokens.first}")
-    else
-      commentable_type = commentable_type.singularize.classify
-      @app = main_app
-    end
+  def load_commentable
+    # /<commentable_type>/:id/comments/...
+    commentable_type, commentable_id = request.path.split('/')[1..2]
+    commentable_type = commentable_type.singularize.classify
     @commentable = commentable_type.constantize.find(commentable_id)
   end
 
