e1cf606d1239d3cd0abf1592f5188418e9899f32
Li Dong <dongli@lasg.iap.ac.cn>
2016-01-07 11:04:33 +0800

    增加动态的分页
    不知道为什么在activity model中设置paginates_per不管用，而直接在controller中设置per管用

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity
-      .order('created_at desc')
-      .where(owner_id: current_user.following_ids, owner_type: 'User')
+    @activities = PublicActivity::Activity.order('created_at desc').where(owner_id: current_user.following_ids, owner_type: 'User').page(params[:page])
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -16,5 +16,4 @@
 #
 
 class Activity < ActiveRecord::Base
-
 end
