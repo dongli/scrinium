b23bbd1cc9ac65c96a8efe976d373ad255c857a7
Li Dong <dongli@lasg.iap.ac.cn>
2016-01-13 16:00:24 +0800

    修改group和topic的权限
    group全部为公开，而隐私的topic不被外部看到。

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -3,6 +3,10 @@ module GroupsHelper
     current_user and not (current_user.groups & groups).empty?
   end
 
+  def is_in_group? group
+    current_user and current_user.groups.include? group
+  end
+
   def get_valid_groups_of user
     user.memberships.where(host_type: 'Group', status: 'approved').map { |x| x.host }
   end
